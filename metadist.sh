a9e7db8d7d83bce27b4aefaa837c0fc1a73b0bb1
Louis Acresti <louis.acresti@gmail.com>
2015-03-30 19:49:29 +0800

    meta..

--- metadist.sh
+++ metadist.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Builds a list of commands to run that will quickly build all the revisions you need...
-git log --pretty="mkdir -p 'metadist/%h' && cp meta/gulpfile.coffee . && cp meta/index.html src/. && rm -rf dist && git checkout %h && gulp build && cp -r dist/* 'metadist/%h'" | tac
+echo "cp -r meta _meta"
+git log --pretty="mkdir -p 'metadist/%h' && cp _meta/gulpfile.coffee . && cp _meta/index.html src/. && rm -rf dist && git checkout %h && gulp build && cp -r dist/* 'metadist/%h'" | tac
