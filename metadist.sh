c99759b83f2b4922282484b97af3863555154de9
Louis Acresti <louis.acresti@gmail.com>
2015-04-02 14:43:24 +0800

    fixing metadist.sh

--- metadist.sh
+++ metadist.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Builds a list of commands to run that will quickly build all the revisions you need...
+# HACK:
+# Builds a list of commands to run that will quickly build all revisions of the "game".
+# A folder called "metadist" will be created that contains all the built revisions.
 echo "mkdir -p ./meta"
 echo "cp -r gulpfile.coffee package.json src/index.html meta/."
-git log 254cab4 --pretty="cp meta/index.html 'metadist/%h/.' # %s" | tac
+git log 254cab4 --pretty="mkdir -p 'metadist/%h' && rm -rf dist && git checkout -f %h && cp meta/gulpfile.coffee meta/package.json . && gulp build && cp -r dist/* 'metadist/%h' && cp meta/index.html 'metadist/%h/.' # %s" | tac
+echo git checkout -f master
